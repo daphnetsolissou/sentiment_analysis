9d7985eebd31596667bef2a389e85901252c678e
Daphne <daphnetsolissou@gmail.com>
2021-12-13 20:21:18 +0200

    Make articles dataframe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 from get_news import NewsDownloader
+import pandas as pd
 
 
 def parse_arguments(args):
@@ -11,18 +12,25 @@ def parse_arguments(args):
     return parsed_args
 
 
-def report_sources():
+def report_sources(input_args):
     pass
+
+
+def download_articles(input_args):
+    downloader = NewsDownloader(input_args.query)
+    articles_list = downloader.get_articles_list()
+    if isinstance(articles_list, list):
+        articles_df = pd.DataFrame(articles_list)
+        articles_df['source'] = articles_df['source'].apply(lambda x: x['name'])
+        return articles_df
+    else:
+        raise sys.exit(-1)
 
 
 def main():
     input_args = parse_arguments(sys.argv[1:])
-    downloader = NewsDownloader(input_args.query)
-    articles_list = downloader.get_articles_list()
-    if isinstance(articles_list, list):
-        pass
-    else:
-        raise sys.exit(-1)
+    if len(sys.argv) == 2:
+        report_sources(input_args)
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,21 @@
-from main import parse_arguments
+import pytest
+import pandas as pd
+
+from main import parse_arguments, download_articles
 
 
-def test_parse_arguments():
-    parser = parse_arguments(['topic', 'brexit'])
-    assert parser.topic == 'topic'
-    assert parser.query == 'brexit'
+@pytest.fixture()
+def argsparser():
+    return parse_arguments(['topic', 'brexit'])
+
+
+def test_parse_arguments(argsparser):
+    # parser = parse_arguments(['topic', 'brexit'])
+    assert argsparser.topic == 'topic'
+    assert argsparser.query == 'brexit'
+
+
+def test_download_articles(argsparser):
+    articles = download_articles(argsparser)
+    if isinstance(articles, pd.DataFrame):
+        assert len(articles.columns) > 0
